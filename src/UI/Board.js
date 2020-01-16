da78a54abb19da1d433345f341be2fb1ab6d645b
pereman2 <pere-altea@hotmail.com>
2020-01-16 21:38:01 +0100

    Lineas fluidas

--- src/UI/Board.js
+++ src/UI/Board.js
@@ -12,9 +12,20 @@ class Board extends React.Component{
         super(props, context)
         this.state = {
           isDown: false,
+          mode: 'brush',
         }
     }
-
+    saveLast = (event, x, y) => {
+        let canvas = document.querySelector('#Board');
+        var pos = getMousePos(canvas, event);
+        var rect = canvas.getBoundingClientRect();
+        var mouseX = x || (pos.x - rect.left);
+        var mouseY = y || (pos.y - rect.top);
+        this.setState({ 
+            lastX: mouseX,
+            lastY: mouseY,
+        });
+    }
     componentDidMount() {
         let canvas = document.querySelector('#Board');
         canvas.addEventListener('mousedown', (e) => this.mouseDown(e), false);
@@ -23,16 +34,22 @@ class Board extends React.Component{
     }
 
     mouseDown = event => {
-        let canvas = document.querySelector('#Board');
-        var pos = getMousePos(canvas, event);
         this.setState({ isDown: true });
-        console.log(this.state.isDown);
+        let canvas = document.querySelector('#Board');
+        this.setState({
+            ctx: canvas.getContext('2d'),
+            begin: true
+        });
+        this.state.ctx.beginPath();
+        this.saveLast(event);
     }
 
     mouseUp = event => {
         let canvas = document.querySelector('#Board');
         var pos = getMousePos(canvas, event);
         this.setState({ isDown: false });
+        //this.state.ctx.stroke();
+        this.state.ctx.closePath();
     }
 
     mouseMove = event => {
@@ -40,12 +57,20 @@ class Board extends React.Component{
             let canvas = document.querySelector('#Board');
             var pos = getMousePos(canvas, event);
             var rect = canvas.getBoundingClientRect();
-
-            let ctx = canvas.getContext('2d');
-            ctx.beginPath();
-            ctx.arc(pos.x - rect.left, pos.y - rect.top, 9, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.stroke();
+            var mouseX = pos.x - rect.left;
+            var mouseY = pos.y - rect.top;
+            var lastX = this.state.lastX;
+            var lastY = this.state.lastY;
+            if(this.state.begin) {
+                this.state.ctx.moveTo(mouseX, mouseY);
+                this.setState({ begin: false });
+            }
+            this.state.ctx.lineTo(lastX, lastY);
+            this.state.ctx.lineJoin = 'round';
+            this.state.ctx.lineCap = 'round';
+            this.state.ctx.lineWidth = 15;
+            this.state.ctx.stroke();
+            this.saveLast(event, mouseX, mouseY);
         }
     }
 
@@ -58,7 +83,7 @@ class Board extends React.Component{
     render() {
         return (
             <div className="Board">
-                <canvas id="Board" onMouseMove={(e) => this.draw(e)} height='700px' width='700px'></canvas>
+                <canvas id="Board" height='700px' width='700px'></canvas>
                 <button onClick={(e) => this.reset(e)}>Reset</button>
             </div>
             
