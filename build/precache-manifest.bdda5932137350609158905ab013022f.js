62ae24844a04f35ce9803ee6008a62df9189c2ea
pereman2 <pere-altea@hotmail.com>
2020-04-05 22:10:46 +0200

    xd

---
+++ build/precache-manifest.bdda5932137350609158905ab013022f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b84adfb287a85e5e836184169614f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37659553fc6c4e982a3e",
+    "url": "./static/css/main.2022e744.chunk.css"
+  },
+  {
+    "revision": "15afb8c5083aba555d22",
+    "url": "./static/js/2.8c75a664.chunk.js"
+  },
+  {
+    "revision": "37659553fc6c4e982a3e",
+    "url": "./static/js/main.ff01d02d.chunk.js"
+  },
+  {
+    "revision": "40f534297450be6e73e8",
+    "url": "./static/js/runtime-main.59765bcc.js"
+  },
+  {
+    "revision": "d3ae2f8f49208490f784aa46f15ee71d",
+    "url": "./static/media/Fondo.d3ae2f8f.png"
+  },
+  {
+    "revision": "a4d3b14fbcc5b8f112d3635f981d6a1c",
+    "url": "./static/media/picker_mask.a4d3b14f.png"
+  }
+]);
